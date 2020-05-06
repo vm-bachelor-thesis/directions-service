0fdf762d57583526be80c05963d3d32e001e5a8f
Viktor <viktor@intrykk.se>
2020-05-06 11:39:37 +0200

    Add text response upload function

---
+++ uploadTextResponse/index.ts
@@ -0,0 +1,21 @@
+import { AzureFunction, Context, HttpRequest } from '@azure/functions';
+
+const httpTrigger: AzureFunction = async function (
+  context: Context,
+  req: HttpRequest,
+): Promise<void> {
+  if (req.body && req.body.response) {
+    context.bindings.responseDocument = {
+      ...req.body.response,
+      type: 'text',
+    };
+
+    context.done();
+  } else {
+    context.res = {
+      status: 400,
+    };
+  }
+};
+
+export default httpTrigger;
